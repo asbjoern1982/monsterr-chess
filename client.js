43b8860ab690c2c0ad3a07c42e913ca4df0a5e9a
asbjoern1982@gmail.com <asbjoern1982@gmail.com>
2018-08-24 14:19:53 +0200

    added game folder

--- client.js
+++ client.js
@@ -1,9 +1,8 @@
 import createClient from 'monsterr'
 import stage1 from './src/stages/stage1/client'
+import chessStage from './src/stages/chess/client/client'
 
-const stages = [
-  stage1
-]
+const stages = [stage1, chessStage]
 
 let options = {
   canvasBackgroundColor: 'blue',
--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 import createServer, { Network } from 'monsterr'
 import stage1 from './src/stages/stage1/server'
+import chessStage from './src/stages/chess/server/server'
 
-const stages = [stage1]
+const stages = [stage1, chessStage]
 
 let events = {}
 let commands = {}
